233f5ecaa6bdfcc946b66b31fa65e32644e91326
IyyappanPalanivel <iyyappan.p@technotackle.com>
2024-05-07 21:21:59 +0530

    header & footer added

--- src/components/blogs/BlogDetail.tsx
+++ src/components/blogs/BlogDetail.tsx
@@ -1,20 +1,21 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import logo from 'assets/brand/logo.png';
+import { NavLink, useParams } from 'react-router-dom';
+import logo from 'assets/brand/logo_transparent.png';
 import { getBlogDetailsById } from '../../datas/BlogDatas';
 import Markdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
+import Footer from '../Footer';
 
 const BlogDetail = (props) => {
 
     // const {blogData} = props;
     const { id } = useParams();
 
-    const [blogData,setBlogData] = useState();
-    const [body,setBody] = useState();
-    
+    const [blogData, setBlogData] = useState();
+    const [body, setBody] = useState();
+
     // Split the description into an array of points based on newline character (\n)
-    
+
 
     useEffect(() => {
         console.log("Id", id)
@@ -24,31 +25,55 @@ const BlogDetail = (props) => {
         setBody(points);
     }, [])
 
-    return (
-        <div className='px-6 md:px-20 mt-10'>
-            {
-                blogData && <>
-                    <h3 className='text-red-500 text-[12px]'>{blogData.title}</h3>
-                    <h3 className='text-[23px] font-bold'>{blogData.title}</h3>
-                    <div className='flex items-center mt-5'>
-                        <img src={logo}
-                            className='w-[35px] rounded-full border' />
-                        <div className='ml-2'>
-                            <h3 className='font-bold text-[12px]'>Mentorhints</h3>
-                            <h3 className='text-gray-500 text-[10px]'>07 May 2024</h3>
-                        </div>
-                    </div>
-                    <img src={blogData.image} className='rounded-2xl mt-5 mb-5 w-full' />
-                    {/* Render each point as a separate paragraph */}
-                    {body.map((point, index) => (
-                        <p key={index} className="text-xs font-poppins font-normal text-gray pb-2">{point}</p>
-                    ))}
-                </>
-            }
+    const Header = () => {
+        return (
+            <header className='w-full bg-brandPrimary flex justify-around items-center p-2 fixed top-0 left-0 right-0'>
+                {/* Brand MentorHints Logo */}
+                <a href='/' className='text-2xl font-semibold flex items-center space-x-3'>
+                    <img className='w-40 h-12 object-cover' src={logo} alt='' />
+                </a>
 
-            {/* <h3>{post.desc}</h3> */}
-            {/* <Markdown remarkPlugins={[remarkGfm]}>{markdown}</Markdown> */}
+                {/* NavItems for Large Devices */}
+                {/* <ul className='md:flex space-x-12 hidden'>
+                    <NavLink className='block cursor-pointer text-sm font-poppins font-normal text-navTextColor hover:text-brandPrimary' to="/">Home</NavLink>
+                    <NavLink className='block cursor-pointer text-sm font-poppins font-normal hover:text-brandPrimary text-brandPrimary' to="/about">About</NavLink>
+                </ul> */}
+            </header>
+        )
+    }
+
+
+    return (
+        <div>
+            <Header/>
+            <div className='px-6 md:px-20 mt-20'>
+                {
+                    blogData && <>
+                        <h3 className='text-red-500 text-[12px]'>{blogData.title}</h3>
+                        <h3 className='text-[23px] font-bold'>{blogData.title}</h3>
+                        <div className='flex items-center mt-5'>
+                            <img src={logo}
+                                className='w-[35px] rounded-full border' />
+                            <div className='ml-2'>
+                                <h3 className='font-bold text-[12px]'>Mentorhints</h3>
+                                <h3 className='text-gray-500 text-[10px]'>07 May 2024</h3>
+                            </div>
+                        </div>
+                        <img src={blogData.image} className='rounded-2xl mt-5 mb-5 w-full' />
+                        {/* Render each point as a separate paragraph */}
+                        {body.map((point, index) => (
+                            <p key={index} className="text-xs font-poppins font-normal text-gray pb-2">{point}</p>
+                        ))}
+                    </>
+                }
+
+                {/* <h3>{post.desc}</h3> */}
+                {/* <Markdown remarkPlugins={[remarkGfm]}>{markdown}</Markdown> */}
+            </div>
+            <hr className="h-px mt-8 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+            <Footer/>
         </div>
+
     );
 };
 
